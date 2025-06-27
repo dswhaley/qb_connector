d584b98dcd0a9f6574ccb87ca39b53fc8a7f5a1f
dswhaley <whaleyds22@gcc.edu>
2025-06-27 10:45:42 -0400

    updated the qb doctypes

---
+++ qb_connector/qb/doctype/shipment_tracker/shipment_tracker.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, funfangle and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Shipment Tracker", {
+// 	refresh(frm) {
+
+// 	},
+// });
