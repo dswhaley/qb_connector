7faab0259671ce09b4889a1fa681eb4335964e7d
dswhaley <whaleyds22@gcc.edu>
2025-06-27 10:30:18 -0400

    Added doctypes

---
+++ qb_connector/qb_connector/doctype/state_tax_information/state_tax_information.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, funfangle and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("State Tax Information", {
+// 	refresh(frm) {
+
+// 	},
+// });
