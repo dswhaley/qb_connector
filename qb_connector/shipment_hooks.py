69006a047fb021a104b958772e4bec2dfd28d687
dswhaley <whaleyds22@gcc.edu>
2025-07-12 14:23:41 -0400

    Shipment now works with Camps and Other Organizations

--- qb_connector/shipment_hooks.py
+++ qb_connector/shipment_hooks.py
@@ -8,16 +8,36 @@ def create_shipment_tracker(doc, method):
         return
     customer = frappe.get_doc("Customer", customer_name)
     shipmentName = f"{customer_name} order {doc.name}"
-    camp_link = customer.custom_camp_link or ""
+
+    organization_type = None
+    if customer.custom_camp_link:
+        organization_type = "Camp"
+    elif customer.custom_other_organization_link:
+        organization_type = "Other Organization"
+    else:
+        frappe.msgprint(f"Customer {customer.name} does not have an organization type")
+        return
+
     tracker = frappe.new_doc("Shipment Tracker")
     tracker.name = shipmentName
     tracker.sales_order = doc.name
+    tracker.organization_type = organization_type
     tracker.shipment_status = "Sales Order Made"
     tracker.shipment_name = shipmentName
-    tracker.camp = customer.custom_camp_link
+    tracker.organization = customer.name
+    set_organization_info(tracker, organization_type, customer.name)
     
     tracker.insert()
     frappe.db.commit()
+
+def set_organization_info(tracker, organization_type, customer_name):
+    organization = frappe.get_doc(organization_type, name=customer_name)
+    tracker.order_id = organization.organization_order_id
+    tracker.shipping_line_1 = organization.shipping_address_1
+    tracker.shipping_line_2 = organization.shipping_address_2
+    tracker.shipping_line_3 = organization.shipping_address_3
+    
+
 
 def link_invoice_to_tracker(doc, method):
     if not doc.items or not doc.items[0].sales_order:
